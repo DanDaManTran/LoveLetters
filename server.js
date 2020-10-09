c4bbb6e7896633bc58e11806eda238acd0b66c4e
DanDaManTran <DanD.Tran89@gmail.com>
2020-10-08 20:22:43 -0500

    adding logs and little more bugs

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ cron.schedule('*/1 * * * *', async () => {
 	// (async() =>{		
   try{
   		const wordsToSend = phrases.splice(Math.floor(Math.random()*phrases.length), 1);
+		console.log(`Sending, ${wordsToSend}`);
 
 		await mailer.sendMail({
 			from: config.mailConfig.auth.user, 
 			to: config.toEmail,
-			subject: `Daily Mail ${pharses.length} to go.`,
+			subject: `Daily Mail ${phrases.length} to go.`,
 			html: htmlCreator(wordsToSend[0]),
 		});
+
+		console.log("Sent");
 	} catch (err) {
 
 		throw err
